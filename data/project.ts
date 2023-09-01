af35182cb13a2dd2cb3d302dac292f5463b83162
BlueOrgreen <1936341390@qq.com>
2023-09-01 19:33:46 +0800

    fix: 图片问题

--- data/project.ts
+++ data/project.ts
@@ -26,7 +26,7 @@ export const projects: Project[] = [
         title: '帆云小站',
         description: '🦖 基于 Docusaurus 静态网站生成器实现个人博客',
         website: "https://www.chenchar.cn/",
-        preview: 'http://s0amp2hkw.hn-bkt.clouddn.com/project.png?e=1693562188&token=pMdVBb-vwl8GbyBzH24u-teppeKCddbSTSKkoE6d:cyTuGAfguzvOdywGW9d9BREDVlw=',
+        preview: 'https://yunfan-cra.oss-cn-shanghai.aliyuncs.com/fanyun-website/blog.png',
         source: "https://www.baidu.com",
         tags: ['opensource', 'design', 'favorite'],
         type: 'web',
@@ -35,7 +35,7 @@ export const projects: Project[] = [
     {
         title: '小雯工作室官网',
         description: '🗼 来自法国设计工作室的官网',
-        preview: 'http://s0amp2hkw.hn-bkt.clouddn.com/project1.png?e=1693562417&token=pMdVBb-vwl8GbyBzH24u-teppeKCddbSTSKkoE6d:1doMFrbgLHmuX7YjDnztkfYondY=',
+        preview: 'https://yunfan-cra.oss-cn-shanghai.aliyuncs.com/fanyun-website/france.png',
         website: 'https://france.chenchar.cn/',
         tags: ['business', 'design', 'personal'],
         type: 'web',
